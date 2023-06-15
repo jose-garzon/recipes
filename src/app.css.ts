539d9825025bff2337f5b4a0579edcbbea6fd2b7
root <root@DESKTOP-OHC60I5>
2023-06-15 17:45:44 -0500

    create the theme and the main styles

--- src/app.css.ts
+++ src/app.css.ts
@@ -1,8 +1,30 @@
 import { globalStyle } from "@vanilla-extract/css";
+import { vars } from "./shared/theme";
 
 globalStyle("*", {
   fontFamily: '"Montserrat", sans-serif',
   padding: 0,
   margin: 0,
   boxSizing: "border-box",
+});
+
+globalStyle("h1", {
+  fontSize: vars.fontSize.h1,
+  color: vars.color.fontDark,
+});
+globalStyle("h2", {
+  fontSize: vars.fontSize.h2,
+  color: vars.color.fontDark,
+});
+globalStyle("h3", {
+  fontSize: vars.fontSize.h3,
+  color: vars.color.fontDark,
+});
+globalStyle("p", {
+  fontSize: vars.fontSize.p,
+  color: vars.color.font,
+});
+globalStyle("small", {
+  fontSize: vars.fontSize.small,
+  color: vars.color.font,
 });
--- src/shared/theme/theme.css.ts
+++ src/shared/theme/theme.css.ts
@@ -1,7 +1,7 @@
 import { createGlobalTheme } from "@vanilla-extract/css";
 import { vars } from "./contract.css";
 
-export const mainTheme = createGlobalTheme(":root", vars, {
+createGlobalTheme(":root", vars, {
   color: {
     brand: "#333333",
     secondary: "#666666",
@@ -9,9 +9,9 @@ export const mainTheme = createGlobalTheme(":root", vars, {
     error: "#CCCCCC",
     success: "#E5E5E5",
     warning: "#F2F2F2",
-    font: "#AAAAAA",
-    fontDark: "#FFFFFF",
-    fontLight: "#000000",
+    font: "#595959",
+    fontDark: "#000000",
+    fontLight: "#FFFFFF",
   },
   spacing: {
     none: "0px",
